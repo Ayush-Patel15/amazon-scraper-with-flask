387b21a3dda1356faf8a5693e9bc42217afbd754
Ayush Patel <patelayushap1512@gmail.com>
2021-05-27 01:15:47 +0530

    detailed scraper function added

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -1,5 +1,5 @@
 import requests
-from requests.compat import urljoin
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import random
 from user_agents_list import USER_AGENTS
@@ -21,7 +21,7 @@ def get_soup(url,headers= {}, params=[]):
     )
     return soup
 
-def scrape(query):
+def get_links(query):
     response = get_soup(
         AMAZON_BASE_URL,
         params=[("k",query)]
@@ -32,5 +32,49 @@ def scrape(query):
         links.append(tag["href"])
     return links
 
+def get_details(link):
+    response = get_soup(
+        urljoin(AMAZON_BASE_URL, link)
+    )
+    try:
+        details_section = response.select('#centerCol')[0]
+        url = urljoin(AMAZON_BASE_URL,link)
+        title = details_section.select('#productTitle')[0].text.strip()
+        brand = details_section.select('#bylineInfo')[0].text
+        availability = details_section.select("#availability")[0].text.strip()
+        description = details_section.select('#feature-bullets')[0].select(".a-list-item")
+        emi = details_section.select("#inemi_feature_div")[0].select("span")[0].text
+        sold_by = details_section.select("#sellerProfileTriggerId")[0].text
+        rating_stars = details_section.select("#acrPopover")[0]["title"]
+        total_raitngs = details_section.select("#acrCustomerReviewText")[0].text
+        if details_section.select('#priceblock_ourprice'):
+            price = details_section.select('#priceblock_ourprice')[0].text
+        elif details_section.select('#priceblock_dealprice'):
+            price = details_section.select('#priceblock_dealprice')[0].text
+        else:
+            price = 'NA'
+        try:
+            price = price.replace("\xa0","")
+        
+        except Exception:
+            price = price
+
+    except Exception:
+        pass
+
+    item = {
+        "title": title,
+        "store/brand": brand,
+        "price": price,
+        "emi": emi.strip(),
+        "description": [each.text.strip() for each in description],
+        "availability": availability,
+        "seller": sold_by,
+        "ratings": f"{rating_stars}, total {total_raitngs}",
+        "url": url
+    }
+    return item
+
 if __name__ == "__main__":
-    pprint(scrape("shoes"))
+    # links = get_links("laptops")
+    pprint(get_details("https://www.amazon.in/HP-Pentium-Processor-15-6-inch-15s-du1052tu/dp/B08HJZHTM1/ref=sr_1_5?dchild=1&keywords=laptops&qid=1622057076&sr=8-5"))
