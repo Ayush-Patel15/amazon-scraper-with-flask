737ed8569acb7d0d6a69088a6e3d5dbc3ae08aa0
Ayush Patel <patelayushap1512@gmail.com>
2021-06-04 13:29:21 +0530

    read value from chrome search bar and home.html search bar

--- scraper/flask_server.py
+++ scraper/flask_server.py
@@ -1,23 +1,38 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, json, jsonify, request, render_template
 from amazon_scraper import get_links, get_details
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("search.html")
+    return render_template("home.html")
 
-@app.route("/search")
+@app.route("/search",methods=["GET","POST"])
 def search():
-    query = request.args["query"]
-    page = request.args["page"]
-    links = get_links(query)
-    if len(links) > 0:
-        data = get_details(links,page)
-        return jsonify(data)
-    else:
-        return jsonify({"status": "Failed","msg":"try for any other query please!!"})
+    try:
+        query = request.form.get("search")
+        if query:
+            page = 1
+            links = get_links(query)
+            if len(links) > 0:
+                data = get_details(links,page)
+                return jsonify(data)
+        else:
+            query = request.args["query"]
+            page = request.args["page"]
+            links = get_links(query)
+            if len(links) > 0:
+                data = get_details(links,page)
+                return jsonify(data)
+    except Exception as e:
+        return jsonify({
+            "status": "Failed",
+            "msg":"try for any other query please!!",
+            "exception": e
+            })
+                
+# todo: Error handling
+
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
-
