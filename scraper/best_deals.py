f7ca361830fa85a52674eaf0e7123c7bbd1888f0
Ayush Patel <patelayushap1512@gmail.com>
2021-06-05 23:04:54 +0530

    added section for best deals on home page

--- scraper/best_deals.py
+++ scraper/best_deals.py
@@ -9,17 +9,20 @@ def deals_of_the_day():
     links_tag = []
     deals_link = []
     images_link = []
-    response = get_soup(BEST_DEALS_URL)
-    deals_section = response.select(".product-shoveler")
-    for deals in deals_section:
-        links_tag.append(deals.select("a"))
-    links = random.choice(links_tag)
-    # slicing is to remove unwanted hrefs
-    for link in links[:-2]:
-        deals_link.append(link["href"])
-        images_link.append(link.select("img")[0]["src"])
-    items = list(zip(deals_link,images_link))
-    return items
+    try:
+        response = get_soup(BEST_DEALS_URL)
+        deals_section = response.select(".product-shoveler")
+        for deals in deals_section:
+            links_tag.append(deals.select("a"))
+        links = random.choice(links_tag)
+        # slicing is to remove unwanted hrefs
+        for link in links[:-2]:
+            deals_link.append(link["href"])
+            images_link.append(link.select("img")[0]["src"])
+        items = list(zip(deals_link,images_link))
+        return items
+    except Exception:
+        deals_of_the_day()
 
 if __name__ == "__main__":
     print(deals_of_the_day())
